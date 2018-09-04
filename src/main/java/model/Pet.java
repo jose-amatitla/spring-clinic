cfe6b9c953b48afda82337369f70bdc45559ec76
Jose Amatitla <jamatitla@gmail.com>
2018-09-04 13:20:43 -0500

    Adding POJOs. It closes #2

---
+++ src/main/java/model/Pet.java
@@ -0,0 +1,26 @@
+package model;
+
+public class Pet {
+	private String name;
+	private Owner owner;
+	private PetType petType;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Owner getOwner() {
+		return owner;
+	}
+	public void setOwner(Owner owner) {
+		this.owner = owner;
+	}
+	public PetType getPetType() {
+		return petType;
+	}
+	public void setPetType(PetType petType) {
+		this.petType = petType;
+	}
+}

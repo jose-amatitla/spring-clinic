27e510b05dce50240bd9cf492108dc3433da3e35
Jose Amatitla <jamatitla@gmail.com>
2018-09-04 12:49:31 -0500

    Initial setup

---
+++ src/main/java/com/spring/clinic/springclinic/SpringClinicApplication.java
@@ -0,0 +1,12 @@
+package com.spring.clinic.springclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringClinicApplication.class, args);
+	}
+}

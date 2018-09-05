6e102a2e7755c134ef4fc62f4217a58594f7f784
Jose Amatitla <jamatitla@gmail.com>
2018-09-05 18:28:53 -0500

    Added list of vets to its index page. It closes #17

--- spring-clinic-web/src/main/java/com/spring/clinic/springclinic/controllers/VetController.java
+++ spring-clinic-web/src/main/java/com/spring/clinic/springclinic/controllers/VetController.java
@@ -1,13 +1,27 @@
 package com.spring.clinic.springclinic.controllers;
 
+import java.util.Set;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.spring.clinic.springclinic.model.Vet;
+import com.spring.clinic.springclinic.service.VetService;
+
 @Controller
-public class VetController {
+public class VetController {	
+	private final VetService vetService;
+	
+	public VetController(VetService vetService) {
+		this.vetService = vetService;
+	}
 
 	@GetMapping({"/vets","/vets/index"})
-	public String listAllVets() {
+	public String listAllVets(Model model) {
+		Set<Vet> vets = vetService.findAll();
+		model.addAttribute("vets", vets);
+		
 		return "vets/index";
 	}	
 }

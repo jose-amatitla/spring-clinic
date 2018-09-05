25f0034b9f3f89752a9fc3de2c51a10bbf4084d5
Jose Amatitla <jamatitla@gmail.com>
2018-09-05 18:13:19 -0500

    List all owners on its index page. It closes #16

--- spring-clinic-web/src/main/java/com/spring/clinic/springclinic/controllers/OwnerController.java
+++ spring-clinic-web/src/main/java/com/spring/clinic/springclinic/controllers/OwnerController.java
@@ -1,13 +1,27 @@
 package com.spring.clinic.springclinic.controllers;
 
+import java.util.Set;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.spring.clinic.springclinic.model.Owner;
+import com.spring.clinic.springclinic.service.OwnerService;
+
 @Controller
-public class OwnerController {
+public class OwnerController {	
+	private final OwnerService ownerService;
+	
+	public OwnerController(OwnerService ownerService) {
+		this.ownerService = ownerService;
+	}	
 	
 	@GetMapping({"/owners","/owners/index"})
-	public String listOwners() {
+	public String listOwners(Model model) {
+		Set<Owner> owners = ownerService.findAll();
+		model.addAttribute("owners", owners);
+		
 		return "owners/index";
 	}
 }

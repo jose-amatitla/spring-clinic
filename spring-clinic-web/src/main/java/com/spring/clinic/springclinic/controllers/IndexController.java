a63702af166fcce36ea56eef37b032fa94d12f82
Jose Amatitla <jamatitla@gmail.com>
2018-09-05 12:48:09 -0500

    Added Vet controller and index page. Moved IndexController to package controller. It closes #7

--- spring-clinic-web/src/main/java/com/spring/clinic/springclinic/controllers/IndexController.java
+++ spring-clinic-web/src/main/java/com/spring/clinic/springclinic/controllers/IndexController.java
@@ -1,4 +1,4 @@
-package com.spring.clinic.springclinic;
+package com.spring.clinic.springclinic.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;

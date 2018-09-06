c096fa43849e1bdb8b65004b22984d074c80fbd0
Jose Amatitla <jamatitla@gmail.com>
2018-09-06 16:42:27 -0500

    Added Specialty entity. Added Specialties property to Vet entity. It closes #25

--- spring-clinic-data/src/main/java/com/spring/clinic/springclinic/model/Vet.java
+++ spring-clinic-data/src/main/java/com/spring/clinic/springclinic/model/Vet.java
@@ -1,5 +1,7 @@
 package com.spring.clinic.springclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person{
 
 	/**
@@ -7,4 +9,13 @@ public class Vet extends Person{
 	 */
 	private static final long serialVersionUID = 1L;
 
+	private Set<Specialty> specialties;
+
+	public Set<Specialty> getSpecialities() {
+		return specialties;
+	}
+
+	public void setSpecialities(Set<Specialty> specialities) {
+		this.specialties = specialities;
+	}
 }

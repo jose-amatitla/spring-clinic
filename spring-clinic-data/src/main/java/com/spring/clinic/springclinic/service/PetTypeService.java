ee26abd585fe651e8061caf9c9161d74dde8d803
Jose Amatitla <jamatitla@gmail.com>
2018-09-06 17:01:53 -0500

    Added PetType Service Map implementation. It closes #27

---
+++ spring-clinic-data/src/main/java/com/spring/clinic/springclinic/service/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.spring.clinic.springclinic.service;
+
+import com.spring.clinic.springclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long>{
+}
---
+++ spring-clinic-data/src/main/java/com/spring/clinic/springclinic/service/mapimpl/PetTypeServiceMapImpl.java
@@ -0,0 +1,37 @@
+package com.spring.clinic.springclinic.service.mapimpl;
+
+import java.util.Set;
+
+import org.springframework.stereotype.Service;
+
+import com.spring.clinic.springclinic.model.PetType;
+import com.spring.clinic.springclinic.service.PetTypeService;
+
+@Service
+public class PetTypeServiceMapImpl extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+	@Override
+	public PetType findById(Long id) {
+		return super.findById(id);
+	}
+
+	@Override
+	public Set<PetType> findAll() {
+		return super.findAll();
+	}
+
+	@Override
+	public PetType save(PetType object) {
+		return super.save(object);
+	}
+
+	@Override
+	public void delete(PetType object) {
+		super.delete(object);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		super.deleteById(id);
+	}
+}

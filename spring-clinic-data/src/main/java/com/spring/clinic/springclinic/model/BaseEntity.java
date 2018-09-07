f15468dac1f94aaef73b32e86b3de96567bc2fb3
Tec Web <jamatitla@gmail.com>
2018-09-07 11:00:51 -0500

    Implemented JPA for BaseEntity class. Closes #33

--- spring-clinic-data/src/main/java/com/spring/clinic/springclinic/model/BaseEntity.java
+++ spring-clinic-data/src/main/java/com/spring/clinic/springclinic/model/BaseEntity.java
@@ -2,6 +2,12 @@ package com.spring.clinic.springclinic.model;
 
 import java.io.Serializable;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
 	/**
@@ -9,6 +15,8 @@ public class BaseEntity implements Serializable {
 	 */
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	public Long getId() {
